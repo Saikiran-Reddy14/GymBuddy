fbfb86bc4fb0e9d5f28b7a8152e3c2bcef6762d8
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2024-05-16 15:15:31 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Box } from "@mui/material";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Box>
+        <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={HomePage} />
+            <Route path="/about" Component={ExerciseDetail} />
+            {/* <Route path="*" Component={InvalidPage} /> */}
+          </Routes>
+        </BrowserRouter>
+        <h1>hello</h1>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
